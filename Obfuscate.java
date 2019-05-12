c3bebf57ef599a4d452b71d5322c4ee93af6d8b4
Matt Kleven <klevennb115@gmail.com>
2019-05-11 19:31:40 -0500

    inputs working

--- Obfuscate.java
+++ Obfuscate.java
@@ -1,20 +1,74 @@
 import java.util.*;
 
-public class Objuscate {
-    str wordOne;
-    str wordTwo;
-    str wordThree;
-    static void checkIfFits(int height, int width) {
-        if (height <= 45 && width <= 60) {
-            throw new ArithmeticException("To Small: Will Fall Out");
-        } else {
-            System.out.println("Big Nuff");
-        }
-    }
+public class Obfuscate {
+
+    // static void checkIfFits(int height, int width) {
+    //     if (height <= 45 && width <= 60) {
+    //         throw new ArithmeticException("To Small: Will Fall Out");
+    //     } else {
+    //         System.out.println("Big Nuff");
+    //     }
+    // }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Weigh-In process!!");
-        checkIfFits(100, 39);
-        System.out.println("Have a nice day..");
+        String wordOne = new String("");
+        String wordTwo = new String("");
+        String wordThree = new String("");
+        ArrayList<Character> letters = new ArrayList<Character>();
+        letters.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+        //will need ^v these
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            // Process char
+        }
+
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter your first word");
+        wordOne = myObj.nextLine();
+        System.out.println("Enter your second word");
+        wordTwo = myObj.nextLine();
+        System.out.println("Enter your third word");
+        wordThree = myObj.nextLine();
     }
 }
+
+// import java.util.*;
+
+// public class Calculator {
+
+//     public static void main(String[] args) {
+//         int num1 = 0;
+//         int num2 = 0;
+//         char operator;
+//         double answer = 0.0;
+
+//         Scanner scanObject = new Scanner(System.in);
+//         System.out.println("Enter your first number");
+//         num1 = scanObject.nextInt();
+//         System.out.println("Enter your second number");
+//         num2 = scanObject.nextInt();
+//         System.out.println("What operator?");
+//         operator = scanObject.next().charAt(0);
+//         switch (operator) {
+//         case '+':
+//             answer = num1 + num2;
+//             break;
+//         case '-':
+//             answer = num1 - num2;
+//             break;
+//         case '*':
+//             answer = num1 * num2;
+//             break;
+//         case '/':
+//             answer = num1 / num2;
+//             break;
+//         }
+//         System.out.println(num1 + " " + operator + " " + num2 + " = " + answer);
+//         ;
+
+//     }
+// }
